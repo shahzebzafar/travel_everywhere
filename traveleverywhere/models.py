e3feaedc8acd695435d8812b47402df75caa4ec5
shahzebzafar <shahzebzafar33@gmail.com>
2020-03-14 04:14:33 +0000

    Shahzeb: Add blog and userprofile models, and edited question and answer models.

--- traveleverywhere/models.py
+++ traveleverywhere/models.py
@@ -5,15 +5,58 @@ from django.contrib.auth.models import User
 class Question(models.Model):
     title = models.URLField(max_length = 50)
     body = models.CharField(max_length = 500)
-    replies = models.IntegerField(default=0)
+    replies = models.IntegerField(default = 0)
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Question, self).save(*args, **kwargs)
+    
+    class Meta:
+        verbose_name_plural = 'Questions'
 
     def __str__(self): 
         return self.title
 
 class Answer(models.Model):
-    text = models.CharField(max_length=500)
-    question = models.ForeignKey(Question, on_delete = models.CASCADE)
+    text = models.CharField(max_length = 500)
+    question = models.ForeignKey(Question, on_delete = models.CASCADE, related_name = 'question')
+    user = models.ForeignKey(User, on_delete = models.CASCADE, related_name = 'user')
 
     def __str__(self): 
         return self.text
 
+class Blog(models.Model):
+    title = models.CharField(max_length = 50)
+    body = models.CharField(max_length = 10000)
+    likes = models.IntegerField(default = 0)
+    publish_date = models.DateField.auto_now_add()
+    location_country = models.CharField(max_length = 20)
+    location_city = models.CharField(max_length = 20, blank = True)
+    location_place = models.CharField(max_length = 50, blank = True)
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Blog, self).save(*args, **kwargs)
+    
+    class Meta:
+        verbose_name_plural = 'Blogs'
+    
+    def __str__(self):
+        return self.title
+        
+class Blog_Image(models.Model):
+    image = models.ImageField(upload_to = 'blog_images', blank = True)
+    blog = models.ForeignKey(Blog, on_delete = models.CASCADE)
+    
+    def __str__(self):
+        return self.image
+                
+class User_Profile(models.Model):
+    user = models.OneToOneField(User, on_delete = models.CASCADE)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+    about = models.CharField(max_length = 500)
+    
+    def __str__(self):
+        return self.user.username
