ffb10884ad5a35b701872a28e15350da86b4735d
GerganaStIvanova <2395634I@student.gla.ac.uk>
2020-04-01 13:57:12 +0100

    Gergana: added tests for some models, forms and views.

--- traveleverywhere/tests.py
+++ traveleverywhere/tests.py
@@ -1,6 +1,19 @@
+import os
+import re
+import inspect
+import tempfile
+import traveleverywhere.models
+from django.conf import settings
 from django.test import TestCase
-from traveleverywhere.models import Question, Blog, Answer, Airline, Agency, BookingWebsite, User, AirlineLike, AirlineDislike, AgencyLike, AgencyDislike, WebsiteLike, WebsiteDislike, BlogLike, Blog_Image
-from django.urls import reverse
+from traveleverywhere.models import Question, Blog, Answer, Airline, Agency, BookingWebsite, AirlineLike, AirlineDislike, AgencyLike, AgencyDislike, WebsiteLike, WebsiteDislike, BlogLike, Blog_Image, User_Profile
+from django.urls import reverse, resolve
+from django.contrib.auth.models import User
+from django.test import Client
+from django.db import models
+from django.forms import fields as django_fields
+from traveleverywhere import forms
+from django.forms import fields as django_fields
+
 
 
 class QuestionModelTests(TestCase):
@@ -30,6 +43,90 @@ class BlogModelTests(TestCase):
         blog.save()
 
         self.assertEqual(blog.slug, "slug-should-have-dashes")
+
+def create_super_user_object():
+    """
+    Helper function to create a super user (admin) account.
+    """
+    return User.objects.create_superuser('admin', 'admin@test.com', 'testpassword')
+
+class UserProfileModelTests(TestCase):
+    
+    def test_user_profile_class_in_models(self):
+        """
+        Checks whether the User_Profile class exists in traveleverywhere models
+        and if the attributes exist as we want them to.
+        If we can't assign values to some of them, assertion fails.
+        """
+        self.assertTrue('User_Profile' in dir(traveleverywhere.models))
+        user_profile = User_Profile()
+        expected_attributes = {
+            'user':add_user('test', 'test123'),
+            'picture': tempfile.NamedTemporaryFile(suffix=".jpg").name,
+            'about' : 'I come from Test file.'
+        }
+        expected_types = {
+            'user': models.fields.related.OneToOneField,
+            'picture':models.fields.files.ImageField,
+            'about':models.fields.CharField,
+        }
+        count = 0
+        for attr in user_profile._meta.fields:
+            attr_name = attr.name
+            for expected_attr_name in expected_attributes.keys():
+                if expected_attr_name == attr_name:
+                    count += 1
+                    self.assertEqual(type(attr), expected_types[attr_name])
+                    setattr(user_profile, attr_name, expected_attributes[attr_name])
+        self.assertEqual(count, len(expected_attributes.keys()))
+        user_profile.save()
+
+    def test_model_admin_creation(self):
+        """
+        Checks for User_Profile interface instance.
+        If we don't get Http 200, assume models have not been registered.
+        """
+        super_user = create_super_user_object()
+        self.client.login(username='admin', password='testpassword')
+        response = self.client.get('/admin/traveleverywhere/user_profile/')
+        self.assertEqual(response.status_code, 200)
+
+class UserUserProfileFormTests(TestCase):
+
+    def test_user_form(self):
+        """
+        Checks if UserForm is in the correct place and has correct fields.
+        """
+        self.assertTrue('UserForm' in dir(forms))
+        user_form = forms.UserForm()
+        self.assertEqual(type(user_form.__dict__['instance']), User)
+        fields = user_form.fields
+        expected_fields = {
+            'username': django_fields.CharField,
+            'email': django_fields.EmailField,
+            'password': django_fields.CharField,
+        }
+        for expected_field_name in expected_fields:
+            expected_field = expected_fields[expected_field_name]
+            self.assertTrue(expected_field_name in fields.keys())
+            self.assertEqual(expected_field, type(fields[expected_field_name]))
+
+    def test_user_profile_form(self):
+        """
+        Checks if UserProfileForm is in the correct place and has correct fields.
+        """
+        self.assertTrue('UserProfileForm' in dir(forms))
+        user_profile_form = forms.UserProfileForm()
+        self.assertEqual(type(user_profile_form.__dict__['instance']), traveleverywhere.models.User_Profile)
+        fields = user_profile_form.fields
+        expected_fields = {
+            'about':django_fields.CharField,
+            'picture':django_fields.ImageField,
+        }
+        for expected_field_name in expected_fields:
+            expected_field = expected_fields[expected_field_name]
+            self.assertTrue(expected_field_name in fields.keys())
+            self.assertEqual(expected_field, type(fields[expected_field_name]))
 
 def add_question(title):
     question = Question.objects.get_or_create(title=title)[0]
@@ -412,8 +509,117 @@ class BlogViewTests(TestCase):
         self.assertContains(response, "Trip to Japan")
         self.assertContains(response, "When in Rome")
 
-
+    def test_blog_view_shows_body_summary(self):
+        """
+        Checks whether the body summary is displayed correctly on the blog page.
+        """
+        user1 = add_user('john', 'travel123')
+        blog_info = {
+        'title' : 'Glasgow – Former Murder Capital of Europe Today',
+        'country' : 'UK',
+        'city' : 'Glasgow',
+        'place' : '',
+        'body' : 'Not many people these days are aware of Glasgow’s infamous reputation as one of the most dangerous cities in the world where crime and violence were at an all-time high, gangs reigned supreme and knifing was a daily occurrence.\nFast forward to today and you’ll find that the city’s gone through a metamorphosis – putting much of its dark past behind it and becoming one of the more attractive places to live in the UK, provided of course you don’t linger around to seedier parts of the city after dark.\nSo what does this once-dangerous city offer to visitors? Quite a lot really. Divided into two halves by the River Clyde, Glasgow is a city with a blend of modern and classic Victorian architecture with many great spots for tourists to visit and enjoy.\nFor this little blog, I’ll focus mainly on the City Centre since that’s where you’ll spend a significant portion of your time. Fancy a shopping spree? There’s plenty of shopping centres and plazas with every possible store imaginable. Want a great dining experience? You’ll be hard pressed to not find a restaurant to suit your palate. Want to go on a romantic stroll with your significant other? An evening walk along the River Clyde is a breath-taking experience. Simply want a nice pint of the finest brew? We’ve got more pubs and bars than the number of people living here. Seriously, it feels like that sometimes.\nSo where to begin? You can start off by taking a train from where you’re staying to Glasgow Central. While a bus or a taxi are also good, I feel every visitor to Glasgow should visit the station at least once. There you’ll find yourself in the heart of the City Centre. Go north on Renfield Street for an amazing dining and theatre experience or west on Gordon Street and enter Buchanan Street for an excellent range of stores and the ever-popular Buchanan Galleries.\nYou can also south on Union Street and link up with Argyle Street which opens up even more avenue for you to explore. If you’re feeling lucky, you can visit one of the many casino’s which dot the City Centre and win big. But do try to not go broke since you still need to pay to go back home. Or if you’d rather just laze about and relax, there are many parks and gardens scattered around and anyone of them will fulfil your needs.\nMost importantly though, if you’re new to the UK and Glasgow is your first time ever in a British city, you have to indulge in a hot sausage roll at Greggs for they are divine and refusing to do so is akin to a crime against the crown. I’m not joking. We have special police to make sure you partake in such a divine delicacy. There’s a vegan option as well for those non-meat eating aliens among us.\nThat should cover the basics. If you have any questions, you can post them in the forum and I’ll do my best to answer them if you do your best and try to visit this reformed city that I’ve come to love.\n',
+        }
+        blog = add_blog_fully(blog_info['title'], blog_info['body'], blog_info['body'][:200] + "...", blog_info['country'], '', blog_info['city'], user1)
         
+        response = self.client.get(reverse('traveleverywhere:blogs'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Glasgow – Former Murder Capital of Europe Today')
+        self.assertEquals(response.context['blogs'][0][1], blog.bodySummary)
+
+class LoginViewTests(TestCase):
+
+    def test_user_login_correctly(self):
+        """
+        Checks that when user is logged in, he is transferred to home page.
+        """
+        user1 = add_user('geri', '1234')
+        client = Client()
+        self.client.login(username = user1.username, password = '1234')
+        response = self.client.post(reverse('traveleverywhere:home'), {'user_id':user1.id})
+        self.assertEqual(response.status_code, 200)
+    
+def get_template(path_to_template):
+    """
+    Helper function to return the string representation of a template file.
+    """
+    f = open(path_to_template, 'r')
+    template_str = ""
+    for line in f:
+        template_str = f"{template_str}{line}"
+    f.close()
+    return template_str
+
+class SignupViewTests(TestCase):
+
+    def test_signup_view_exists(self):
+        """
+        Checks if the signup view exists in the correct place.
+        """
+        url = ''
+        try:
+            url = reverse('traveleverywhere:signup')
+        except:
+            pass
+        self.assertEqual(url, '/traveleverywhere/signup/')
+
+    def test_signup_template(self):
+        """
+        Checks if the signup.html exists in he correct place and
+        if it uses the template inheritance.
+        """
+        template_base_path = os.path.join(settings.TEMPLATE_DIR, 'traveleverywhere')
+        template_path = os.path.join(template_base_path, 'signup.html')
+        self.assertTrue(os.path.exists(template_path))
+        template_str = get_template(template_path)
+        title_pattern = r'{% block title_block %}(\s*|\n*)Sign Up - TravelEverywhere(\s*|\n*){% (endblock|endblock title_block) %}'
+        self.assertTrue(re.search(title_pattern, template_str))
+
+    def test_signup_get_response(self):
+        """
+        Checks the GET response of signup view.
+        """
+        request = self.client.get(reverse('traveleverywhere:signup'))
+        content = request.content.decode('utf-8')
+        self.assertTrue('<h1 class="text-center signup-title">Sign up for TravelEverywhere</h1>' in content)
+        self.assertTrue('enctype="multipart/form-data"' in content)
+        self.assertTrue('<button type="submit" class="btn btn-primary">Sign up</button>' in content)
+
+    def test_form_creation(self):
+        """
+        Creates a UserProfileForm and UserForm and attempts to save them.
+        Checks if we can login in with the details submited.
+        """
+        user_data = {'username': 'testuser', 'password': 'test123', 'email': 'test@test.com'}
+        user_form = forms.UserForm(data=user_data)
+        user_profile_data = {'about': 'I am a Test user.', 'picture': tempfile.NamedTemporaryFile(suffix=".jpg").name}
+        user_profile_form = forms.UserProfileForm(data=user_profile_data)
+        self.assertTrue(user_form.is_valid())
+        self.assertTrue(user_profile_form.is_valid())
+        user_object = user_form.save()
+        user_object.set_password(user_data['password'])
+        user_object.save()
+        user_profile_object = user_profile_form.save(commit=False)
+        user_profile_object.user = user_object
+        user_profile_object.save()
+        self.assertEqual(len(User.objects.all()), 1)
+        self.assertEqual(len(User_Profile.objects.all()), 1)
+        self.assertTrue(self.client.login(username='testuser', password='test123'))
+
+    def test_signup_post_response(self):
+        """
+        Checks the POST response of the signp view.
+        We should be able to log a user with the details.
+        """
+        post_data = {'username': 'webformuser', 'password': 'test123', 'email': 'test@test.com', 'about': 'I am a Test user.', 'picture': tempfile.NamedTemporaryFile(suffix=".jpg").name}
+        request = self.client.post(reverse('traveleverywhere:signup'), post_data)
+        content = request.content.decode('utf-8')
+        self.assertTrue('<h1 class="text-center signup-title">Sign up for TravelEverywhere</h1>' in content)
+        self.assertTrue('<h2>Thank you for signing up!</h2>' in content)
+        self.assertTrue('Return to homepage.' in content)
+        self.assertTrue(self.client.login(username='webformuser', password='test123'))
+
 
 
     
